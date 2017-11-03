ffd97bc0c9debf9dfca760a0db4f28b6bd520198
joginaidu <fayazbasha974@gmail.com>
2017-11-03 19:39:21 +0530

    done

--- src/app/shared/pipes/search.pipe.ts
+++ src/app/shared/pipes/search.pipe.ts
@@ -1,14 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({name: 'search'})
+@Pipe({name: 'search', pure:false})
 export class SearchPipe implements PipeTransform {
   transform(value: any, args: any): any {
     let source : any = [];
-    //if (!value) return value;
-    if(args != undefined){
+    if (!value) return value;
+    if(args.Hours_Guarnteed != undefined){
         value.filter(eachValue => {
             if(eachValue){
-                if(eachValue.Hours_Guaranteed == args){
+                if(eachValue.Hours_Guaranteed == args.Hours_Guarnteed){
                     source.push(eachValue);
                 }
             }

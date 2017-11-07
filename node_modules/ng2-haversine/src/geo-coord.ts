3ac73989fe2f938e5417b13f3d72bc011c88fed8
joginaidu <fayazbasha974@gmail.com>
2017-11-07 10:56:38 +0530

    done

--- node_modules/ng2-haversine/src/geo-coord.ts
+++
@@ -1,4 +0,0 @@
-export interface GeoCoord {
-    latitude: number;
-    longitude: number;
-}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url:'/https://velai.herokuapp.com/'
+  api_url:'/'
 };
